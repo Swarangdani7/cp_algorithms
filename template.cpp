6c025551dae662cede6589a5dd42878cc45d53f0
Swarang Dani <55289811+Swarangdani7@users.noreply.github.com>
2021-06-07 13:54:28 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#define ll long long 
+#define mp make_pair
+#define pb push_back
+#define ppb pop_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " "; _pr(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+void _pr(int t) {cerr << t;}
+void _pr(ll t) {cerr << t;}
+void _pr(string t) {cerr << t;}
+void _pr(char t) {cerr << t;}
+void _pr(double t) {cerr << t;}
+
+template<class T>void _pr(vector<T> t){cerr<<'[';for(T x:t){_pr(x);cerr<<',';}cerr<<']';}
+template<class T>void _pr(set<T> t){cerr<<'{';for(T x:t){_pr(x);cerr<<',';}cerr<<'}';}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+        freopen("error.txt","w",stderr);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    return 0;
+}
